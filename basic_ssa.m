8fb7aef9993dae1a40d7484bc895cb8f7c9c1e9a
Kevin Shi <xpspectre@gmail.com>
2017-10-30 15:28:46 -0400

    Basic ssa with final figs Figs 1-3: 3 different system sizes: ODE and a few SSA traces Fig 4: Overlaid std devs of many SSA traces

--- basic_ssa.m
+++ basic_ssa.m
@@ -1,15 +1,12 @@
 function basic_ssa
-% Basic reversible binding model, i.e., protein + ligand <-> complex
+%% Basic reversible binding model, i.e., protein + ligand <-> complex
 % P + L <-> C
 clear; close all; clc
-rng('default');
+% rng('default');
 
 % Rate constants
 kon = 1e5; % (1/(M*s))
 koff = 1e-2; % (1/s)
-
-% Compartment volume
-V = 1e-15; % (L) ~E. coli cell volume
 
 % Avogadro's number
 nA = 6.022e23; % count/mol
@@ -19,44 +16,107 @@ P0 = 3e-7; % (M)
 L0 = 1e-6; % (M)
 C0 = 0; % (M)
 
-c0 = [P0, L0, C0]; % in concs
-x0 = round(c0*nA*V); % in counts
-
 % Set simulation conditions
-tf = 100;
+tf = 100; % (s)
 
-% Run ODE simulation
-[tOde, cOde] = ode15s(@ode_model, [0, tf], c0');
-yOde = cOde*nA*V;
+% Compartment volume
+% V = 1e-15; % (L) 1e-15 ~E. coli cell volume
+% V = 1e-17;
 
-% Run stochastic simulation
-%   Each run implicitly uses a different RNG seed
-nRuns = 5;
-ts = cell(nRuns,1);
-xs = cell(nRuns,1);
-for i = 1:nRuns
-    [tSsa, xSsa] = ssa_sim(tf, x0);
-    ts{i} = tSsa;
-    xs{i} = xSsa;
+%% Sims at different system sizes: same concs, different volumes
+% Using same conc, different volume is easy (single param) and makes y-axes in
+%   concs automatically the same
+% OTOH, using same volume, different conc may be easier to conceptualize but
+%   requires scaling y-axis and modifying all x0's.
+useConcs = true; % if false, use counts
+
+nRuns = 1000; % per volume
+nShowTraces = 3;
+
+Vs = [1e-15, 1e-16, 1e-17];
+nV = length(Vs);
+
+tOdes = cell(nV,1);
+xOdes = cell(nV,1);
+tSsass = cell(nV,1);
+xSsass = cell(nV,1);
+for iV = 1:nV
+    V = Vs(iV);
+    [tOde, xOde, tSsas, xSsas] = run_sims(V, nRuns);
+    
+    if useConcs
+        xOde = xOde./(nA*V);
+        for i = 1:nRuns
+            xSsas{i} = xSsas{i}./(nA*V);
+        end
+    end
+    
+    tOdes{iV} = tOde;
+    xOdes{iV} = xOde;
+    tSsass{iV} = tSsas;
+    xSsass{iV} = xSsas;
+    
+    figure
+    plot(tOde, xOde, 'LineWidth', 2)
+    hold on
+    ax = gca;
+    for i = 1:nShowTraces
+        ax.ColorOrderIndex = 1;
+        stairs(tSsas{i}, xSsas{i})
+    end
+    hold off
+    xlabel('Time (s)')
+    if useConcs
+        ylabel('Conc (M)')
+    else
+        ylabel('Count')
+    end
+    title(sprintf('P + L <-> C, V = %.2g L\nCounts: P0 = %i, L0 = %i', V, round(P0*nA*V), round(L0*nA*V)))
+    xlim([0, tf])
+    legend('P','L','C','Location','best')
 end
 
-% Plot trajectories
+%% Overlaid +/- 1 std dev for each system size
+% Interpolate at query points
+nt = 100;
+ts = linspace(0, tf, nt)';
+
 figure
-plot(tOde, yOde, 'LineWidth', 2)
+plot(tOde, xOde, 'LineWidth', 1)
 hold on
 ax = gca;
-for i = 1:nRuns
-    ax.ColorOrderIndex = 1;
-    stairs(ts{i}, xs{i}) % using regular plot here is not as accurate
+ymax = 0; % for setting the ylims
+for iV = 1:nV
+    xs = zeros(nRuns,nt,3);
+    for i = 1:nRuns
+        t = tSsass{iV}{i};
+        x = xSsass{iV}{i};
+        xs(i,:,:) = interp1(t, x, ts, 'previous');
+    end
+    
+    xmean = squeeze(mean(xs, 1));
+    xstd = squeeze(std(xs, 0, 1));
+    xlo = xmean - xstd;
+    xhi = xmean + xstd;
+    
+    ymax = max(ymax, max(xhi(:))); % highest vals in the sim
+    
+    for i = 1:3
+        color = ax.ColorOrder(i,:);
+        fill([ts; flipud(ts)], [xlo(:,i); flipud(xhi(:,i))], color, 'EdgeColor', 'none');
+        alpha(0.25) % overlaid to give differential shading
+    end
+    
 end
 hold off
-xlabel('Time')
-ylabel('Counts')
-title(sprintf('P + L <-> C\nODE = thick, SSA = thin lines'))
+xlabel('Time (s)')
+ylabel('Conc (M)')
+ylim([0, ymax*1.1])
+title(sprintf('P + L <-> C\nODE and SSA with +/-1 std dev for different volumes'))
 xlim([0, tf])
 legend('P','L','C','Location','best')
 
-
+%% Helper functions
     function [ts, xs] = ssa_sim(tf, x0)
         % Gillespie stochastic simulation algorithm
         % States: x1 = P, x2 = L, x3 = C
@@ -81,7 +141,6 @@ legend('P','L','C','Location','best')
                 ts = [ts; zeros(nt,1)];
                 xs = [xs; zeros(nt,nx)];
                 nt = nt * 2;
-%                 fprintf('Extended solution in step %i\n', it)
             end
             
             % Calculate reaction propensities and normalization
@@ -134,6 +193,28 @@ legend('P','L','C','Location','best')
         dx2 = -kon*x(1)*x(2) + koff*x(3);
         dx3 =  kon*x(1)*x(2) - koff*x(3);
         dx = [dx1; dx2; dx3];
+    end
+
+    function [tOde, xOde, tSsas, xSsas] = run_sims(V, nRuns)
+        % Run deterministic and stochastic simulations with different system
+        % sizes (but same concs)
+        c0 = [P0, L0, C0]; % in concs
+        x0 = round(c0*nA*V); % in counts
+        
+        % Run ODE simulation
+        opts = odeset('RelTol',1e-8,'AbsTol',1e-12);
+        [tOde, cOde] = ode15s(@ode_model, [0, tf], c0', opts);
+        xOde = cOde*nA*V;
+        
+        % Run stochastic simulation
+        %   Each run implicitly uses a different RNG seed
+        tSsas = cell(nRuns,1);
+        xSsas = cell(nRuns,1);
+        for iRun = 1:nRuns
+            [tSsa, xSsa] = ssa_sim(tf, x0);
+            tSsas{iRun} = tSsa;
+            xSsas{iRun} = xSsa;
+        end
     end
 
 end
